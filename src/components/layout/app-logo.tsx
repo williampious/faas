68e3a7d1c0f704c5ef7ad253a72106a6bf9da291
William Pious Akakpo <williampious@gmail.com>
2025-06-13 19:49:43 +0000

    the dashboard logo seem to small if you can make it more visible

--- src/components/layout/app-logo.tsx
+++ src/components/layout/app-logo.tsx
@@ -1,10 +1,11 @@
+
 import Image from 'next/image';
 import Link from 'next/link';
 
 export function AppLogo() {
   return (
     <Link href="/" className="flex items-center gap-2 px-2 group">
-      <div className="relative h-12 w-48 md:h-14 md:w-56">
+      <div className="relative h-16 w-48 md:h-20 md:w-60">
         <Image
           src="/agrifaas-logo.png" // Assumes agrifaas-logo.png is in /public
           alt="AgriFAAS Connect Logo"
