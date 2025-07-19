03a136c829d64a2f5285904807dc9fc4b04f7a48
William Pious Akakpo <williampious@gmail.com>
2025-07-19 04:45:21 +0000

    Remaining Warnings (Still Present):
    Additionally, the warnings I pointe

--- src/next.config.ts
+++ src/next.config.ts
@@ -3,9 +3,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    allowedDevOrigins: ["*.cloudworkstations.dev"],
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
